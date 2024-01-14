aa67ca18fbd9a45e8d4ec34d14ed0c35b429f376
Christopher Negron <sonycar2000@outlook.com>
2024-01-13 19:09:00 -0500

    test

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-server.db = router.db;
-
 server.use(middlewares);
-server.use(auth({ users: "users.json" }));
 
 server.use(router);
 
