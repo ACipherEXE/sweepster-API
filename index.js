5aaae97f8d19ce7c436ce4f63f0c45c132f02245
Christopher Negron <sonycar2000@outlook.com>
2024-01-13 19:17:33 -0500

    test

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+const port = process.env.PORT || 8080;
 server.use(middlewares);
 
 server.use(router);
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
