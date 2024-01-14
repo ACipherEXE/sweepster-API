aa4651faa375c7d9aa735fe3e1daf7e9ebc928b5
Christopher Negron <sonycar2000@outlook.com>
2024-01-14 10:52:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("hotels.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || process.env.HTTP_PLATFORM_PORT || 80; //  chose port from here like 8080, 3001
+const port = process.env.PORT || process.env.HTTP_PLATFORM_PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
